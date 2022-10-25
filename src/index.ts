5020088cf47f37ef2a45f06c3895f7e4d6d1eea0
Jelle Scheer <jellescheer@gmail.com>
2022-10-25 09:28:49 +0200

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { setup, axiosInstance } from "./axios";
-import { AxiosInstance } from "axios";
-
-setup({
-  baseUrl: 'https://huddle.test/api/v3/',
-});
-
-export let axiosInstanceQ: AxiosInstance = axiosInstance!;
+console.log('test');
